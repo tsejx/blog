d8acef18e57e7d3e1da7c2099d9d8729b5a6b32a
tsejx <kufong1994@gmail.com>
2020-04-15 01:27:58 +0800

    config: 修改部署配置脚本

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  // pathPrefix: '',
+  pathPrefix: '/blog',
   // polyfill: true,
   // mapping: {},
   // proxy: {},
